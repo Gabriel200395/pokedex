7e951f37c8f50d2a31efd53aeeea25aac85bbd05
Gabriel Souza <eugabrielf@gmail.com>
2022-08-29 16:08:22 -0300

    fazendo conexao api e criando card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Card: React.FC = () => {
+  return <div />;
+}
+
+export default Card;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 
+interface PropsPokenApi {
+  name: string;
+  id: number;
+  order: number;
+  sprites: {
+    back_default: string;
+  };
+}
+
 const Home = () => {
+  const [pokemonAll, setPokemonAll] = useState<PropsPokenApi[]>([]);
+
+  useEffect(() => {
+    async function pokemonApiAll() {
+      let endpoints = [];
+
+      for (let i = 1; i <= 50; i++) {
+        endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      }
+      try {
+        const pokemonData = await axios.all(
+          endpoints.map((endpoint) => axios.get(endpoint))
+        );
+
+        const response = pokemonData.map((i) => i.data);
+        setPokemonAll([...pokemonAll, ...response]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    pokemonApiAll();
+  }, []);
+
+  console.log(pokemonAll);
+
   return (
     <>
       <Navbar />
+      
     </>
   );
 };
